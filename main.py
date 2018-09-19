176a4be76ddd09935e722d01b74b182ae9339e7b
Irwin Tani <itani05@gmail.com>
2018-09-18 21:12:31 -0500

    Setup initial input forms

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+    <!DOCTYPE html>
+
+    <html>
+      <head>
+        <style>
+          form {
+              background-color: #eee;
+              padding: 20px;
+              margin: 0 auto;
+              width: 540px;
+              font: 16px sans-serif;
+              border-radius: 10px;
+          }
+          textarea {
+              margin: 10px 0;
+              width: 540px;
+              height: 120px;
+          }
+        </style>
+      </head>
+      <body>
+        <form method=['POST']>
+          <label for="rot">Rotate by:</label>
+          <input id="rot" type="text" name="rot" value=0 />
+          <input type="textarea" name="text" />
+          <input type="button" value="Submit" />
+
+        </form>
+        
+      </body>
+    </html>
+"""
+@app.route("/")
+def index():
+    return form
+
+app.run()    
